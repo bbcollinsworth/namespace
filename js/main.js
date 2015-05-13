e3c43a60de98a0ac0367c551e1c1a7acbac7e92c
monotoan <bcollinsworth@gmail.com>
2015-05-13 11:40:25 -0400

    Navigation controls working

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ var loaded = false;
 
 var rotateSpeed = 0.001;
 
+var mouseX;
+var mouseY;
+var xNormal;
+var yNormal;
+
 //var xBound;
 //var zBound;
 //var height;
@@ -37,7 +42,60 @@ jQuery(document).ready(function($) {
 
 //---SCENE AND CAMERA
 
+//Thanks to mcode for basis of this function - http://stackoverflow.com/questions/23514274/three-js-2d-text-sprite-labels
+function makeTextTexture(message, parameters) {
+	if (parameters === undefined) parameters = {};
+	var fontface = parameters.hasOwnProperty("fontface") ? parameters["fontface"] : "Arial";
+	var fontsize = parameters.hasOwnProperty("fontsize") ? parameters["fontsize"] : 42;
+	var borderThickness = parameters.hasOwnProperty("borderThickness") ? parameters["borderThickness"] : 0;
+	var borderColor = parameters.hasOwnProperty("borderColor") ? parameters["borderColor"] : {
+		r: 0,
+		g: 0,
+		b: 0,
+		a: 1.0
+	};
+	var backgroundColor = parameters.hasOwnProperty("backgroundColor") ? parameters["backgroundColor"] : {
+		r: 255,
+		g: 255,
+		b: 255,
+		a: 0.0
+	};
+	var textColor = parameters.hasOwnProperty("textColor") ? parameters["textColor"] : {
+		r: 255,
+		g: 255,
+		b: 255,
+		a: 1.0
+	};
+
+	var canvas = document.createElement('canvas');
+	var context = canvas.getContext('2d');
+	context.font = "Bold " + fontsize + "px " + fontface;
+	var metrics = context.measureText(message);
+	var textWidth = metrics.width;
+
+	context.shadowColor = "white";
+	context.shadowBlur = 50;
+
+	context.fillStyle = "rgba(" + backgroundColor.r + "," + backgroundColor.g + "," + backgroundColor.b + "," + backgroundColor.a + ")";
+	context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + "," + borderColor.b + "," + borderColor.a + ")";
+
+	context.lineWidth = borderThickness;
+	//roundRect(context, borderThickness / 2, borderThickness / 2, (textWidth + borderThickness) * 1.1, fontsize * 1.4 + borderThickness, 8);
+
+	context.fillStyle = "rgba(" + textColor.r + ", " + textColor.g + ", " + textColor.b + ", 1.0)";
+	context.fillText(message, borderThickness, fontsize + borderThickness);
+
+	var texture = new THREE.Texture(canvas);
+	texture.needsUpdate = true;
+
+	return texture;
+}
+
 function threeJSinit(d) {
+
+	window.addEventListener('resize', onWindowResize, false);
+
+	var renderStarted;
 
 	console.log("Post-AJAX log: ");
 	console.log(d);
@@ -45,7 +103,7 @@ function threeJSinit(d) {
 	var scene = new THREE.Scene();
 	var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 	//var controls = new THREE.OrbitControls( camera );
-  	//controls.addEventListener( 'change', render );
+	//controls.addEventListener( 'change', render );
 
 	//---RENDERER SETTINGS
 	var renderer = new THREE.WebGLRenderer();
@@ -80,19 +138,38 @@ function threeJSinit(d) {
 	var renderedNames = [];
 
 	var endSprite = THREE.ImageUtils.loadTexture(
-			"images/endSpriteB.png"
-		);
+		"images/endSpriteB.png"
+	);
 
 	var lineSprite = THREE.ImageUtils.loadTexture(
-			"images/lineSpriteB.png"
-		);
+		"images/lineSpriteB.png"
+	);
+
+	var endPointMaterials = [];
 
 	//for (var x = -xBound; x <= xBound; x++) {
 	for (var x = 0; x < gridWidth; x++) {
 		//START WITH X, THEN ADD THIS:
 		//var height = Math.random() * 3;
 
-		console.log("x: " + x);
+		//console.log("x: " + x);
+
+		var endPointParticles = new THREE.Geometry();
+
+		var nameSprite = makeTextTexture(d.maleNames[x].name);
+
+		endPointMaterials[x] = new THREE.PointCloudMaterial({
+			size: 1, //was 2
+			map: nameSprite,
+			blending: THREE.AdditiveBlending,
+			depthTest: false,
+			transparent: true
+		});
+
+		var hslAdjust = x / gridWidth;
+
+		endPointMaterials[x].color.setRGB(1.0, hslAdjust, 0.0);
+		//endPointMaterials[x].color.setHSL(1.0, hslAdjust, 1.0);
 		//for (var z = -zBound; z <= zBound; z++) {
 		for (var z = 1914; z < 2014; z++) {
 
@@ -102,7 +179,7 @@ function threeJSinit(d) {
 			for (var statsIndex in d.maleNames[x].stats) {
 				if (d.maleNames[x].stats[statsIndex].year == z) {
 					height = d.maleNames[x].stats[statsIndex].count;
-					console.log("Name: " + d.maleNames[x].name + ", Year: " + d.maleNames[x].stats[statsIndex].year + ", Height: " + height);
+					//console.log("Name: " + d.maleNames[x].name + ", Year: " + d.maleNames[x].stats[statsIndex].year + ", Height: " + height);
 					break;
 				}
 			}
@@ -111,28 +188,27 @@ function threeJSinit(d) {
 
 			//=====UPDATING X AND Z==========
 			var gridSpacing = 0.8;
-			var fX = (x - gridWidth * 0.5) * gridSpacing*2;
-			var fZ = (z - 1914) * gridSpacing*2;
+			var fX = (x - gridWidth * 0.5) * gridSpacing * 1;
+			var fZ = (z - 1914) * gridSpacing * 2;
 			//var y = height;
-			var particle = new THREE.Vector3(fX, height, fZ); //-d.maleNames.length/2
-			console.log("Particle Height: " + particle.y);
-			//var particle = new THREE.Vertex(vert);
+			if (height > 0) {
+				var particle = new THREE.Vector3(fX, height, fZ); //-d.maleNames.length/2
+				//console.log("Particle Height: " + particle.y);
+				//var particle = new THREE.Vertex(vert);
 
-			particles.vertices.push(particle);
+				//particles.vertices.push(particle);
+				endPointParticles.vertices.push(particle);
+			}
 
-			for (var i = 0.5; i<height; i+=0.5){
-				var linePoint = new THREE.Vector3(fX,i,fZ);
+			for (var i = 0.5; i < height; i += 0.5) {
+				var linePoint = new THREE.Vector3(fX, i, fZ);
 				linePoints.vertices.push(linePoint);
 
 			}
 
-			//---SETTING THE COLOR
-			var bldgColor = 0.2 + Math.random() * 4 * 0.1;
-			//COLORS ARE FLOAT COLORS
-			var color = new THREE.Color(bldgColor, bldgColor - Math.random() * 0.05, bldgColor - Math.random() * 0.1);
-
-			
 		}
+		var endSystem = new THREE.PointCloud(endPointParticles, endPointMaterials[x]);
+		scene.add(endSystem);
 	}
 
 
@@ -159,10 +235,11 @@ function threeJSinit(d) {
 		transparent: true
 	});
 
-	var particleSystem = new THREE.PointCloud(particles, pMaterial);
-	var lineSystem = new THREE.PointCloud(linePoints,pLineMaterial);
+	//var particleSystem = new THREE.PointCloud(particles, pMaterial);
+	//scene.add(particleSystem);
 
-	scene.add(particleSystem);
+	var lineSystem = new THREE.PointCloud(linePoints, pLineMaterial);
+
 	scene.add(lineSystem);
 
 	// for (var i = 0; i < renderedNames.length; i++) {
@@ -190,7 +267,12 @@ function threeJSinit(d) {
 	//USING VECTORS HERE... START WITH POSITION X, POSITION Y?
 	camera.up = new THREE.Vector3(0, 1, 0);
 
-	camera.position.set(-7, 3, 7);
+	//camera.rotation.order = 'YXZ';
+
+	//camera.position.set(-7, 3, 7);
+	camera.position.set(0, 20, 0);
+	camera.rotation.set(0, 0, 0);
+	camera.lookAt(new THREE.Vector3(0, 20, midPosZ));
 
 	//TO INCREMENT THINGS...
 	var frameCount = 0;
@@ -198,7 +280,7 @@ function threeJSinit(d) {
 	//THINK OF THIS AS YOUR UPDATE & DRAW FUNCTION
 	//it runs 60 times per second
 	var render = function() {
-
+		renderStarted = true;
 		//controls.update();
 
 		//REMEMBER THAT THIS IS X,Z, NOT Z,Y
@@ -214,10 +296,39 @@ function threeJSinit(d) {
 		// camera.position.z = Math.cos(frameCount*0.001)*7;
 
 		//VECTOR POSITION SETTING:
-		camera.position.set(camRotateX, 30, camRotateZ + 50);
-		//camera.position.set(gridWidth*0.7, 30, midPosZ);
+		//camera.position.set(camRotateX, 30, camRotateZ + 50);
+		//camera.position.set(0, 20, 0);
+		//camera.position.z += ( mouseX - camera.position.z ) * 0.5;
+		//camera.position.x += ( - mouseY - camera.position.x ) * 0.5;
+		//camera.lookAt( scene.position );
+		//camera.lookAt(new THREE.Vector3(0, 30, midPosZ));
+		var zoomFactor;
+		if (yNormal < 0.3) {
+			zoomFactor = mathMap(yNormal, 0, 0.3, -1, 0);
+		} else if (yNormal > 0.7) {
+			zoomFactor = mathMap(yNormal, 0, 0.7, 0, 1);
+		} else {
+			zoomFactor = 0;
+		}
 
-		camera.lookAt(new THREE.Vector3(0, 0, midPosZ));
+		camera.translateZ(zoomFactor);
+
+		var degree = 3.14 / 180;
+		var rotFactor;
+		if (xNormal <= -0.3) {
+			rotFactor = mathMap(xNormal, -0.3, -1, 0, degree * 1.5);
+			rotFactor = -1 * rotFactor;
+		} else if (xNormal >= 0.3) {
+			rotFactor = mathMap(xNormal, 0.3, 1, 0, degree * 1.5);
+		} else {
+			rotFactor = 0;
+		}
+		//var rotFactor = xNormal*3.14/180;
+		console.log("RotFactor: " + rotFactor);
+		camera.rotation.y += rotFactor;
+		console.log("Rotation: " + camera.rotation.y);
+
+		console.log("Cam Pos: " + camera.position.x + ", " + camera.position.y + ", " + camera.position.z);
 
 		var lightDistance = 15;
 		var lightRotateX = Math.sin(frameCount * 0.005) * lightDistance;
@@ -226,8 +337,8 @@ function threeJSinit(d) {
 		sunLight.position.set(0, 100, 0);
 		//console.log(Math.abs(lightRotateY));
 
-		var sunColorG = map(Math.abs(lightRotateY), 0, lightDistance, 0.3, 1);
-		var sunColorB = map(Math.abs(lightRotateY), 0, lightDistance, 0.2, 0.8);
+		var sunColorG = mathMap(Math.abs(lightRotateY), 0, lightDistance, 0.3, 1);
+		var sunColorB = mathMap(Math.abs(lightRotateY), 0, lightDistance, 0.2, 0.8);
 		sunColor = new THREE.Color(1, sunColorG, sunColorB);
 		sunLight.color = sunColor;
 
@@ -236,8 +347,8 @@ function threeJSinit(d) {
 		}
 
 		//---TO CHANGE SKY COLOR
-		var skyColorB = map(Math.abs(lightRotateY), 0, lightDistance, 0.6, 1);
-		var skyColorG = map(Math.abs(lightRotateY), 0, lightDistance, 0.2, 0.8);
+		var skyColorB = mathMap(Math.abs(lightRotateY), 0, lightDistance, 0.6, 1);
+		var skyColorG = mathMap(Math.abs(lightRotateY), 0, lightDistance, 0.2, 0.8);
 
 		//skyColor = new THREE.Color(0.7, skyColorG, skyColorB);
 		//renderer.setClearColor(skyColor, 1);
@@ -255,26 +366,57 @@ function threeJSinit(d) {
 
 	//if (loaded) {
 	render();
+
+
 	//}
+	function onWindowResize() {
 
+		windowHalfX = window.innerWidth / 2;
+		windowHalfY = window.innerHeight / 2;
 
-	function map(varToMap, varMin, varMax, mapToMin, mapToMax) {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
+
+	}
+
+	function mathMap(varToMap, varMin, varMax, mapToMin, mapToMax) {
 		var mappedValue = mapToMin + (mapToMax - mapToMin) * ((varToMap - varMin) / (varMax - varMin));
 		return mappedValue;
 	}
 
 	$(window).mousemove(function(e) {
-		var mouseX = e.pageX;
-		var xNormal = mouseX / window.innerWidth;
-		console.log("MouseX: " + xNormal);
+		mouseX = e.pageX;
+		mouseY = e.pageY;
+		xNormal = mouseX / window.innerWidth;
+		//xNormal = mouseX / window.innerWidth;
+		xNormal = xNormal * 2 - 1;
+		//console.log("Xnormal: " + xNormal);
+		yNormal = mouseY / window.innerHeight;
+		//console.log("MouseX: " + xNormal);
 
-		rotateSpeed = map(xNormal, 0, 1, 0.0001, 0.005);
+		rotateSpeed = mathMap(xNormal, 0, 1, 0.0001, 0.005);
 		// if (yNormal < 0.5){
 		// 	rotateSpeed *=1.1;
 		// } else if (yNormal > 0.5){
 		// 	camera.position.z += 2;
 		// }
 
+	});
+
+	$(document).on('keydown', function(e) {
+		if (e.which == 38) {
+			console.log("keypressed Up");
+			if (camera.position.y < 50) {
+				camera.position.y += 0.5;
+			}
+		} else if (e.which == 40) {
+			console.log("keypressed Down");
+			if (camera.position.y > 0) {
+				camera.position.y -= 0.5;
+			}
+		}
 	});
 }
 
